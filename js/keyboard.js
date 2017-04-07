5f2649351042752c35f3dc90e1c93cc0774c0d4c
Pooja Ashoka <pashoka@cisco.com>
2017-04-07 15:44:03 +0530

    completed onscreen keyboard navigation & updating the input field with selected string

--- js/keyboard.js
+++ js/keyboard.js
@@ -4,23 +4,33 @@ var KEYS = {
   right: 39,
   down: 40,
   ok: 13
-}
-
-function addclass(element, classname){
-  element.className += ' ' + className;
 };
 
-function removeclass(element, classname) {
-  element.className = element.className.replace(' ' + className, '').replace(className, '');
+var char = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+"n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "spc", "clr"];
+
+function addclass(element, classname){
+  //alert("add");
+  element.className = "button" + ' ' + classname;
+};
+
+function removeclass(element) {
+  //alert("remove");
+  element.className = "button";
 };
 
 function initialise() { 
+  var container = document.getElementById("keyboard");
+  for(var i = 0; i < char.length; i++) {
+    if( i == 0 ) container.innerHTML += '<div id="' + char[i] + '"' + 'class="button selected">' + char[i] + '</div>';
+    else container.innerHTML += '<div id="' + char[i] + '"' + 'class="button">' + char[i] + '</div>';
+  }
   document.onkeydown = function  (event) {
 
         switch (event.keyCode) {
             case KEYS.left:
                 // Left
-                alert("left");
                 moveLeft();
                 break;
             case KEYS.up:
@@ -28,7 +38,6 @@ function initialise() {
                 break;
             case KEYS.right:
                 // Right
-                alert("right")
                 moveRight();
                 break;
             case KEYS.down:
@@ -45,29 +54,58 @@ function initialise() {
 
 var cur = 0;
 var max = 12;
-var vertical = 2;
-var hor = 2;
-var list = document.getElementByClass("button");
+var row = 0, rmax = 2;
+var list = document.getElementsByClassName("button");
 
 function moveLeft(){
-    
+    if(cur == 0 || cur == 13 || cur == 26) {
+      return;
+    }
+    console.log(cur);
+    removeclass(list[cur]);
+    cur--;
+    addclass(list[cur], "selected");
 };
 
 function moveRight(){
-        
+    if(cur == max || cur == 25 ||cur == 37){
+      return;
+    } 
+    console.log(cur);
+    removeclass(list[cur]);
+    cur++;
+    addclass(list[cur], "selected");   
 };
 
 function moveUp(){
+    if(row == 0) {
+      return;
+    }
+    row--;
+    removeclass(list[cur]);
+    cur = cur-max-1;
+    console.log("cur ",cur);
+    addclass(list[cur], "selected");
 
 };
 
 function moveDown(){
-
+    if(row == rmax || cur == 25) {
+      return;
+    }
+    row++;
+    removeclass(list[cur]);
+    cur = cur+max+1; 
+    console.log("cur ",cur);
+    addclass(list[cur], "selected");
 };
 
 function select(){
-        alert("okay");
+        //alert("okay");
         var text = $('#ajax');
-        text.val(text.val() + 'a');    
-    
+        var prev = list[cur].id;
+        console.log(prev);
+        if(prev == 'spc') text.val(text.val() + ' ');
+        else if(prev == 'clr') text.val(''); 
+        else text.val(text.val() + prev);   
 };
